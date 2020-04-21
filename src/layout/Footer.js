b97f4932163645ebd1c420bd9a9b7af170ecd533
Được Ngô Hữu <nhd.ars@gmail.com>
2020-04-21 22:17:04 +0700

    second

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -20,15 +20,15 @@ const Footer = () => {
               </form>
           </div>
           <div id="social-network" className={footerStyle.socialIcon}>
-            <ul>
+            <ul className={footerStyle.ul1}>
                 <li>
                     <a href="#"> <img src="https://cdn.shopify.com/s/files/1/0126/6076/8825/files/Group_3_Copy_10_2x_aa6c8b9d-457c-4619-b245-4d5f03b4fb99.png?18896"/></a>
                 </li>
-                <li href="#">
-                    <a> <img src="https://cdn.shopify.com/s/files/1/0126/6076/8825/files/Group_8_Copy_6_2x_e9e7dadb-879a-427c-8a52-bf9a49ef2f8f.png?18901"/></a>
+                <li >
+                    <a href="#"> <img src="https://cdn.shopify.com/s/files/1/0126/6076/8825/files/Group_8_Copy_6_2x_e9e7dadb-879a-427c-8a52-bf9a49ef2f8f.png?18901"/></a>
                 </li>
-                <li href="#">
-                    <a> <img src="https://cdn.shopify.com/s/files/1/0126/6076/8825/files/youtube.png?v=1578106155"/></a>
+                <li >
+                    <a href="#"> <img src="https://cdn.shopify.com/s/files/1/0126/6076/8825/files/youtube.png?v=1578106155"/></a>
                 </li>
             </ul>
           </div>
@@ -36,20 +36,49 @@ const Footer = () => {
         </div>
         <div className="col-md-8 col-sm-12">
          <div className='row'>
-           <div className='col-md-6 col-sm-12'>
+           <div className='col-md-3 col-sm-12'>
               <p className={footerStyle.contactHeading2}>Lối Tắt</p>
-              <ul>
+              <ul className={footerStyle.ul2}>
                 <li>
                   <a href="#">Ghé Cửa Hiệu</a>
                 </li>
                 <li>
-                  <a href="#">Ghé Cửa Hiệu</a>
+                  <a href="#">Thực đơn gợi ý</a>
+                </li>
+                <li>
+                  <a href="#">Chính sách</a>
+                </li>
+                <li>
+                  <a href="#">Tin tức</a>
                 </li>
               </ul>
 
            </div>
-           <div className='col-6'>
+           <div className='col-md-6 col-sm-12'>
                 <p className={footerStyle.contactHeading2}>Liên Hệ</p>
+                <ul className={footerStyle.ul2}>
+                <li>
+                  <a href="#">
+                    <i className={`fa fa-phone mr-3 ${footerStyle.icon}`} aria-hidden="true"></i><span>1982312837</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                  <i className={`fa fa-envelope ${footerStyle.icon}`} aria-hidden="true"></i> <span >info@hieucabien.com</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                  <i className={`fa fa-map ${footerStyle.icon}`} aria-hidden="true"></i> <span >44 Tây Sơn, Tân Quý, Tân Phú, Hồ chí Minh</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                  <i className={`fa fa-clock-o ${footerStyle.icon}`} aria-hidden="true"></i> <span > Thứ 2 — Thứ 7, 8:00am - 5:00pm</span>
+                  </a>
+                </li>
+                
+              </ul>
            </div>
          </div>
 
--- src/layout/Products.js
+++ src/layout/Products.js
@@ -6,7 +6,7 @@ const Products = () => {
        {/* product list */}
       <div id="cá biển nhập khẩu">
       <h2 id="hometitle" className={ProductsStyle.homeTitle}>
-        Cá Biển Nhập Khẩu
+        Cá Biển Khơi Xa (Cá Rạng)
       </h2>
       <div id="product" className={ProductsStyle.container}>
          
@@ -20,7 +20,7 @@ const Products = () => {
           <div className="card-body">
             <h5 className="card-title">Cá Hồng</h5>
             <p className="card-text text-danger">Giá bán: 120,000đ</p>
-            <button className={ProductsStyle.button}>Mua Ngay ></button>
+            <button className={ProductsStyle.button}>Chi Tiết ></button>
           </div>
         </div>
         
@@ -35,7 +35,7 @@ const Products = () => {
           <div className="card-body">
             <h5 className="card-title">Cá Mú</h5>
             <p className="card-text text-danger">Giá bán: 180,000đ</p>
-            <button className={ProductsStyle.button}>Mua Ngay ></button>
+            <button className={ProductsStyle.button}>Chi Tiết ></button>
           </div>
         </div>
         
@@ -50,7 +50,7 @@ const Products = () => {
           <div className="card-body">
             <h5 className="card-title">Cá Chẽm</h5>
             <p className="card-text text-danger">Giá bán: 220,000đ</p>
-            <button className={ProductsStyle.button}>Mua Ngay ></button>
+            <button className={ProductsStyle.button}>Chi Tiết ></button>
           </div>
         </div>
         
@@ -65,7 +65,7 @@ const Products = () => {
           <div className="card-body">
             <h5 className="card-title">Cá Hồng</h5>
             <p className="card-text text-danger">Giá bán: 150,000đ</p>
-            <button className={ProductsStyle.button}>Mua Ngay ></button>
+            <button className={ProductsStyle.button}>Chi Tiết ></button>
           </div>
         </div>
 
@@ -74,9 +74,9 @@ const Products = () => {
       
       </div>
 
-      <div id="cá biển tiện lợi">
+      <div id="Không Phải Cá">
       <h2 id="hometitle" className={ProductsStyle.homeTitle}>
-        Cá Biển Khơi Xa
+        Không Phải Là Cá
       </h2>
       <div id="product" className={ProductsStyle.container}>
          
@@ -84,13 +84,13 @@ const Products = () => {
         <div className="card text-left">
           <img
             className="card-img-top"
-            src="http://www.vanthinhseafoods.com/images/Sanphamseafood/RED%20SQUIRED/Red%20Squirel%20fish.jpg"
+            src="http://www.vanthinhseafoods.com/images/Sanphamseafood/SWIMMING%20CRAB%20-%20Ghe/Three%20Spot%20Crab%201.jpg"
             alt="ca-bien, ca-bien-xuat-khau, ca-bien-khoi-xa."
           />
           <div className="card-body">
-            <h5 className="card-title">Cá Hồng</h5>
+            <h5 className="card-title">Ghẹ Biển</h5>
             <p className="card-text text-danger">Giá bán: 120,000đ</p>
-            <button className={ProductsStyle.button}>Mua Ngay ></button>
+            <button className={ProductsStyle.button}>Chi Tiết ></button>
           </div>
         </div>
         
@@ -99,13 +99,13 @@ const Products = () => {
         <div className="card text-left">
           <img
             className="card-img-top"
-            src="http://www.vanthinhseafoods.com/images/Sanphamseafood/GROUPER/Grouper%20fillet%2001.jpg"
+            src="http://www.vanthinhseafoods.com/images/Sanphamseafood/SWIMMING%20CRAB%20-%20Ghe/DSC01218.jpg"
             alt="ca-bien, ca-bien-xuat-khau, ca-bien-khoi-xa."
           />
           <div className="card-body">
-            <h5 className="card-title">Cá Mú</h5>
+            <h5 className="card-title">Ghẹ Xanh</h5>
             <p className="card-text text-danger">Giá bán: 180,000đ</p>
-            <button className={ProductsStyle.button}>Mua Ngay ></button>
+            <button className={ProductsStyle.button}>Chi Tiết ></button>
           </div>
         </div>
         
@@ -114,13 +114,13 @@ const Products = () => {
         <div className="card text-left">
           <img
             className="card-img-top"
-            src="http://www.vanthinhseafoods.com/images/Sanphamseafood/BARRAMUNDI/Fresh%20Barramundi%20fillet%20skin%20on%2001.jpg"
+            src="http://www.vanthinhseafoods.com/images/Sanphamseafood/CUTTLEFISH%20-%20Muc%20nang/Shell%20cuttlefish02.jpg"
             alt="ca-bien, ca-bien-xuat-khau, ca-bien-khoi-xa."
           />
           <div className="card-body">
-            <h5 className="card-title">Cá Chẽm</h5>
+            <h5 className="card-title">Mực Nang vừa</h5>
             <p className="card-text text-danger">Giá bán: 220,000đ</p>
-            <button className={ProductsStyle.button}>Mua Ngay ></button>
+            <button className={ProductsStyle.button}>Chi Tiết ></button>
           </div>
         </div>
         
@@ -129,13 +129,13 @@ const Products = () => {
         <div className="card text-left">
           <img
             className="card-img-top"
-            src="http://www.vanthinhseafoods.com/images/Sanphamseafood/RED%20SNAPPER/Frozen%20Red%20snapper.jpg"
+            src="http://www.vanthinhseafoods.com/images/Sanphamseafood/OCTOPUS%20-%20Bach%20tuoc/WCO%201skin02.jpg"
             alt="ca-bien, ca-bien-xuat-khau, ca-bien-khoi-xa."
           />
           <div className="card-body">
-            <h5 className="card-title">Cá Hồng</h5>
+            <h5 className="card-title">Bạch Tuột Nhỏ</h5>
             <p className="card-text text-danger">Giá bán: 150,000đ</p>
-            <button className={ProductsStyle.button}>Mua Ngay ></button>
+            <button className={ProductsStyle.button}>Chi Tiết ></button>
           </div>
         </div>
 
@@ -143,6 +143,76 @@ const Products = () => {
       </div>
       
       </div>
+      <div id="Cá Tiện Lợi">
+      <h2 id="hometitle" className={ProductsStyle.homeTitle}>
+        Cá Tiện Lợi (Cá Biển Tẩm Bột)
+      </h2>
+      <div id="product" className={ProductsStyle.container}>
+         
+
+        <div className="card text-left">
+          <img
+            className="card-img-top"
+            src="http://www.vanthinhseafoods.com/images/Sanphamseafood/SWIMMING%20CRAB%20-%20Ghe/Three%20Spot%20Crab%201.jpg"
+            alt="ca-bien, ca-bien-xuat-khau, ca-bien-khoi-xa."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Ghẹ Biển</h5>
+            <p className="card-text text-danger">Giá bán: 120,000đ</p>
+            <button className={ProductsStyle.button}>Chi Tiết ></button>
+          </div>
+        </div>
+        
+   
+
+        <div className="card text-left">
+          <img
+            className="card-img-top"
+            src="http://www.vanthinhseafoods.com/images/Sanphamseafood/SWIMMING%20CRAB%20-%20Ghe/DSC01218.jpg"
+            alt="ca-bien, ca-bien-xuat-khau, ca-bien-khoi-xa."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Ghẹ Xanh</h5>
+            <p className="card-text text-danger">Giá bán: 180,000đ</p>
+            <button className={ProductsStyle.button}>Chi Tiết ></button>
+          </div>
+        </div>
+        
+   
+
+        <div className="card text-left">
+          <img
+            className="card-img-top"
+            src="http://www.vanthinhseafoods.com/images/Sanphamseafood/CUTTLEFISH%20-%20Muc%20nang/Shell%20cuttlefish02.jpg"
+            alt="ca-bien, ca-bien-xuat-khau, ca-bien-khoi-xa."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Mực Nang vừa</h5>
+            <p className="card-text text-danger">Giá bán: 220,000đ</p>
+            <button className={ProductsStyle.button}>Chi Tiết ></button>
+          </div>
+        </div>
+        
+   
+
+        <div className="card text-left">
+          <img
+            className="card-img-top"
+            src="http://www.vanthinhseafoods.com/images/Sanphamseafood/OCTOPUS%20-%20Bach%20tuoc/WCO%201skin02.jpg"
+            alt="ca-bien, ca-bien-xuat-khau, ca-bien-khoi-xa."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Bạch Tuột Nhỏ</h5>
+            <p className="card-text text-danger">Giá bán: 150,000đ</p>
+            <button className={ProductsStyle.button}>Chi Tiết ></button>
+          </div>
+        </div>
+
+      
+      </div>
+      
+      </div>
+      
     </div>
   );
 };
